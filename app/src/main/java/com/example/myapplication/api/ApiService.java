956db33cbfe224855708ea13cd8d007ea590c0a4
Pau Arqueros <pauarqueros27@gmail.com>
2026-03-03 12:13:36 +0100

    Creada UserDto

--- app/src/main/java/com/example/myapplication/api/ApiService.java
+++ app/src/main/java/com/example/myapplication/api/ApiService.java
@@ -1,4 +1,13 @@
 package com.example.myapplication.api;
 
+import java.util.List;
+
+import retrofit2.Call;
+import retrofit2.http.Body;
+import retrofit2.http.GET;
+import retrofit2.http.POST;
 public interface ApiService {
+
+    @GET("users/{id}")
+    Call<UserDto> getUserById(@Path("id") Long id);
 }
---
+++ app/src/main/java/com/example/myapplication/api/UserDto.java
@@ -0,0 +1,4 @@
+package com.example.myapplication.api;
+
+public class UserDto {
+}
